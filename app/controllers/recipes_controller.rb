0ae9d45403d8bd2caf5923b6ddf1e9687e333b19
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-05 16:41:16 -0800

    commit tree e9c79d990d07540f1577cf37e4de7cf054772d04

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,13 +2,39 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @ingredients = Ingredient.all
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
   def show
+    @recipe = Recipe.find_by(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find_by(params[:id])
+    @ingredients = Ingredient.all
+  end
+  def update
+    @recipe = Recipe.find_by(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  
+
   private
+
+  def recipe_params 
+    
+    params.require(:recipe).permit(:name, ingredient_ids: [])
+ 
+  end
 
 end
